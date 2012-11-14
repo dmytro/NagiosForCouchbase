eb47f53e10b7fa95a564efb2738209aed65ace33
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-11-14 16:39:24 +0900

    configuration files layout

--- client/lib/couchbase.rb
+++ client/lib/couchbase.rb
@@ -1,4 +1,8 @@
 require 'pp'
+
+# Some core classes extensions - Object, Array, String.
+require_relative 'extensions'
+
 require_relative 'wizcorp/couchbase/connection'
 require_relative 'wizcorp/couchbase/counters'
 require_relative 'wizcorp/couchbase/buckets'
@@ -7,13 +11,5 @@ require_relative 'wizcorp/couchbase/bucket_stats'
 require_relative 'wizcorp/nagios/api'
 require_relative 'wizcorp/nagios/checks'
 
-# Some core classes extensions - Object, Array, String.
-require_relative 'extensions'
 
-APP = { :lib => File.dirname(__FILE__) }
-APP[:root] = File.dirname APP[:lib] 
-
-CONFIG = YAML.load_file "#{APP[:root]}/config/environment.yml"
-
-
-
+require_relative '../config/environment'
